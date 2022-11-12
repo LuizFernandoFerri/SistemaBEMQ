4e3d284feb39801201d0c886db4bbd0370b98e9e
LuizFernandoFerri <luizfernandorb.fe@gmail.com>
2022-11-11 21:32:55 -0300

    Adicionar arquivos de projeto.

---
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ControleDeContatos.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Nome é obrigatório ")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O numero de Matrícula é obrigatório ")]
+        public string Matricula { get; set; }
+        [Required(ErrorMessage = "O horario de Entrada é obrigatório ")]
+        public DateTime Entrada { get; set; }
+        [Required(ErrorMessage = "O horario de Saída é obrigatório ")]
+        public DateTime Saida { get; set; }
+        public int? UsuarioId { get; set; }
+
+        public UsuarioModel Usuario { get; set; }
+    }
+}
